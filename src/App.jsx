1b7f9c50dc142dd60e11675540e9be523d430b49
Johannes Westlund <johannes@stockfiller.com>
2023-04-03 00:57:56 +0200

    Adding some mui flavor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { CssBaseline, ThemeProvider, Typography } from "@mui/material"
+import { StackedLineChart } from "@mui/icons-material"
+import { AppBar, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material"
 import { createAppTheme } from "./configuration/theme"
+
+const Header = () => (
+  <AppBar position="sticky" sx={{ zIndex: theme => theme.zIndex.drawer + 1}}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          aria-label="Home"
+        >
+          <StackedLineChart />
+        </IconButton>
+        <Typography varaint="h6" noWrap>TechnologyMapr</Typography>
+      </Toolbar>
+    </AppBar>
+)
 
 const App = () => {
 
   return (
     <ThemeProvider theme={createAppTheme()}>
       <CssBaseline>
+        <Header />
         <Typography variant="h1">Techmapr</Typography>
       </CssBaseline>
     </ThemeProvider>
